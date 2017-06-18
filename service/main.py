ac39d7e09dff65dc64f1e440955957197202014a
bix709 <bix709@gmail.com>
2017-06-19 00:08:07 +0200

    Added service declaration to buildozer

--- service/main.py
+++ service/main.py
@@ -5,10 +5,13 @@
 """
 import os
 
+import plyer
+
 from common_notifications.android_notification import notification_service
 from common_utilities.Utilities import ignored
 
 if __name__ == '__main__':
+    plyer.vibrator.vibrate(10000)
     with ignored(Exception):
         session_id = int(os.getenv('PYTHON_SERVICE_ARGUMENT'))
     notification_service(session_id)
