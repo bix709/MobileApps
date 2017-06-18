497c4827094e62947c8ed043bc3ca29d7787724b
bix709 <bix709@gmail.com>
2017-06-18 18:17:22 +0200

    Checking if fix android_notifications applies.

--- service/main.py
+++ service/main.py
@@ -11,4 +11,4 @@ from common_utilities.Utilities import ignored
 if __name__ == '__main__':
     with ignored(Exception):
         session_id = int(os.getenv('PYTHON_SERVICE_ARGUMENT'))
-        notification_service(session_id)
+    notification_service(session_id)
