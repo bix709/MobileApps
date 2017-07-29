7889d86c114f19358101f07bdc0ffa2625283665
bix709 <bix709@gmail.com>
2017-07-29 19:26:58 +0200

    Removed ignoring exceptions comming from service

--- adventureskiing/notification_service/main.py
+++ adventureskiing/notification_service/main.py
@@ -6,9 +6,7 @@
 import os
 
 from adventureskiing.notification_service.notification_service import dispatch_incoming_notifications
-from common_utilities.Utilities import ignored
 
 if __name__ == '__main__':
-    with ignored(Exception):
-        session_id = int(os.getenv('PYTHON_SERVICE_ARGUMENT'))
-        dispatch_incoming_notifications(session_id)
+    session_id = int(os.getenv('PYTHON_SERVICE_ARGUMENT'))
+    dispatch_incoming_notifications(session_id)
