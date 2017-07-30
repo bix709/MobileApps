2634ec853bc7860090a8f88a39782fd9071cffe5
bix709 <bix709@gmail.com>
2017-07-30 12:28:28 +0200

    Added broadcast receiver and alarm manager to trigger notifications when sleeping.

--- adventureskiing/notification_service/main.py
+++ adventureskiing/notification_service/main.py
@@ -5,18 +5,11 @@
 """
 import os
 from time import sleep
-
-import plyer
-
 from adventureskiing.notification_service.notification_service import dispatch_incoming_notifications
 from common_alarms.android_alarm import schedule_alarm_manager
-from common_notifications.android_notification import AndroidNotification
 
 if __name__ == '__main__':
     session_id = int(os.getenv('PYTHON_SERVICE_ARGUMENT'))
-    # schedule_alarm_manager(20, lambda *args: dispatch_incoming_notifications(session_id))
-    schedule_alarm_manager(20, lambda *args: AndroidNotification().notify(title='hi', message='hello', app_name='ASy',
-                                                                          app_icon='adventureskiing/graphics/logo.png',
-                                                                          ticker='hi'))
+    schedule_alarm_manager(20, lambda *args: dispatch_incoming_notifications(session_id))
     while True:
         sleep(10)
